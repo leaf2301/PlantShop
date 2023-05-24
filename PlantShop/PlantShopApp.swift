adab288d52d80d8899fcdffe650f74ca13ff579d
Trần Trung Hiếu <108396149+leaf2301@users.noreply.github.com>
2023-05-24 14:29:37 +0700

    Initial Commit

---
+++ PlantShop/PlantShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantShopApp.swift
+//  PlantShop
+//
+//  Created by Tran Hieu on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
